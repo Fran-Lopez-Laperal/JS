e061ba0056379cd110e917433e1c6f49c95eb171
Fran <fransanxenxo@gmail.com>
2023-01-28 19:18:28 +0100

    ejercicio 7

--- pruebas-logica1/6.mayor-repetido.js
+++ pruebas-logica1/6.mayor-repetido.js
@@ -20,4 +20,4 @@ function countGreater(arr) {
        
 }
 
-console.log(countGreater([1, 12, 26, 3, 4, 26, 5, 3, 26, 1, 8]));
+console.log(countGreater([1, 12, 26, 3, 4, 26, 5, 3, 32, 26, 1, 8]));
--- pruebas-logica1/7.encontrar-duplicados.js
+++ pruebas-logica1/7.encontrar-duplicados.js
@@ -5,6 +5,20 @@ Devuelve un array con todos los elementos que aparezcan al menos dos veces en el
 
 function findDuplicates(arr) {
 
+    let duplicados = [];
+
+    let orderArr = arr.sort()
+
+    for (let i = 0; i < orderArr.length ; i++){
+     
+       if (orderArr[i + 1] === orderArr[i]){
+       
+        duplicados.push(orderArr[i])
+       }
+    }
+    return duplicados
 }
 
 console.log(findDuplicates(["hola", "hola", 1, 1, 5, 6, 1, 1, 6, 8, 12]));
+
+
