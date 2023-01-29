c43f48f98584384aaaab0122ad0d530f79b13650
Fran <fransanxenxo@gmail.com>
2023-01-29 11:10:20 +0100

    ejercicios 11 y 13

--- pruebas-logica1/11.encontrar-unicos.js
+++ pruebas-logica1/11.encontrar-unicos.js
@@ -3,8 +3,26 @@
 Devuelve un array con todos los elementos que aparezcan solo una vez en el array que nos llega por parámetro.
 */
 
-function unique(arr) { 
+function unique(arr) {
+
+
+
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i + 1] = arr[i]) {
+           return  arr[i]
+        }
+
+        // console.log(sortArr[i])
+        // if(arr[i] === arr[i + 1]){
+        //      false
+        // }else if(arr[i] = arr[i + 1]){
+        //     unicos.push(arr[i])
+        // }
+    }
+
+
 
 }
 
-console.log(orderSentence([4, 4, 4, 4, 4]));
+console.log(unique([4, 5, 4, 4, 6]));
--- pruebas-logica1/13.invertir-numero.js
+++ pruebas-logica1/13.invertir-numero.js
@@ -5,6 +5,8 @@ Dado un número, devolverlo invertido.
 
 function invertNumber(num) {
 
+    return num.toString().split("").reverse().join("")
+   
 }
 
 console.log(invertNumber(123456789));
