cf3a5c9d38af686006d282d22c7c5d439f68fa2c
Francisco José López Laperal <fransanxenxo@gmail.com>
2023-05-29 12:36:33 +0200

    ejercicio 24

--- pruebas-logica1/24.string-or-number.js
+++ pruebas-logica1/24.string-or-number.js
@@ -4,6 +4,13 @@ Determinar si el valor recibido (value) es number o string.
 */
 
 function numOrStr(value) {
+    if(typeof value === "number"){
+        return 'El valor es un número'
+    }else if(typeof value === "string"){
+        return 'El valor es un string'
+    }else{
+        return 'El valor no es string ni número'
+    }
 }
 
-console.log(numOrStr(12));
+console.log(numOrStr(Boolean));
