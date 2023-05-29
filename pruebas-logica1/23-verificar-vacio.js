338010d4852a3c09220dcb29a094bef1979e41d6
Francisco José López Laperal <fransanxenxo@gmail.com>
2023-05-29 12:25:00 +0200

    ejercicio 23

--- pruebas-logica1/23-verificar-vacio.js
+++ pruebas-logica1/23-verificar-vacio.js
@@ -4,6 +4,13 @@ Devolver true si el string está vacío, y false si no lo está.
 */
 
 function isEmpty(str) {
+
+    if(str.trim() === ""){
+        return true
+    }else{
+        return false
+    }
+
 }
 
-console.log(isEmpty("hola"));
+console.log(isEmpty(" "));
