17af5ae40ce3b7989c8c0d51a70c7e92a71e3c63
Francisco José López Laperal <fransanxenxo@gmail.com>
2023-05-29 12:17:01 +0200

    ejercicio 22

--- pruebas-logica1/22.horas-segundos.js
+++ pruebas-logica1/22.horas-segundos.js
@@ -4,8 +4,10 @@ Devolver el valor de hours en segundos
 */
 
 function hoursToSeconds(hours) {
+    let seconds = Math.floor(hours * 3600) 
 
+    return seconds
 }
 
-console.log(hoursToSeconds(1));
+console.log(hoursToSeconds(0.1));
 
