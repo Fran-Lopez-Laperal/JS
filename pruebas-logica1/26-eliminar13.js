c87ec354cf25e43ebaaa6ab8e5c92423b000fefa
Francisco José López Laperal <fransanxenxo@gmail.com>
2023-05-29 13:04:01 +0200

    ejercicio 26

--- pruebas-logica1/26-eliminar13.js
+++ pruebas-logica1/26-eliminar13.js
@@ -4,7 +4,15 @@ Vamos a recibir un array, tenemos que eliminar todos los múltiplos de 13 y devo
 */
 
 function removeThirteen(arr) {
+    let newArr = []
 
+ for(let i = 0; i < arr.length; i++){
+   if(arr[i] % 13 != 0){
+     newArr.push(arr[i])
+   }
+ }
+ return newArr
+    
 }
 
 console.log(removeThirteen([12, 53, 182, 435, 591, 637]));
